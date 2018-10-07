3b85fa6566ee8e808201b02ce9201280e2041af5
shohiebsense <shohieb.the.witch@gmail.com>
2018-10-07 21:15:10 +0700

    Creating list

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,52 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+import 'package:flutter_my_first/category.dart';
+import 'package:flutter_my_first/category_route.dart';
 
-void main() => runApp(new MyApp());
+const _categoryName = 'Cake';
+const _categoryIcon = Icons.cake;
+const _categoryColor = Colors.green;
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HelloWidget extends StatelessWidget {
+  Widget getContainer() {
+    return Center(
+      child: Container(
+        color: Colors.blueAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            'Hello',
+            style: TextStyle(fontSize: 40.0),
+          ),
+        ),
+      ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        title: 'Selamat Datang di Flutter',
-        theme: new ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-          // counter didn't reset back to zero; the application is not restarted.
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Selamat Datang di Flutter'),
-          ),
-          body: Center(child: Text('Hello Dunia')),
-        ));
+    return getContainer();
   }
+}
+
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: CategoryRoute(),
+    );
+  }
+
+}
+
+
+void main() {
+  runApp(UnitConverterApp());
 }
